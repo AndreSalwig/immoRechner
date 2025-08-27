3b03667067890383234349d1a0411c75b74992a7
Andre <andre@familiesalwig.de>
2025-08-27 10:17:08 +0200

    Initial Backend Commit

---
+++ immoRechnerBackend/src/main/java/de/salwig/immoRechnerBackend/ImmoRechnerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.salwig.immoRechnerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoRechnerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoRechnerBackendApplication.class, args);
+	}
+
+}
