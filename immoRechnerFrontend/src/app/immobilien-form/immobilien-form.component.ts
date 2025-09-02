cdd57f305ad58cec4996615b5b4cdf8dc2a30add
Andre <andre@familiesalwig.de>
2025-09-02 20:02:51 +0200

    migrate core 20 + cli

--- immoRechnerFrontend/src/app/immobilien-form/immobilien-form.component.ts
+++ immoRechnerFrontend/src/app/immobilien-form/immobilien-form.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-immobilien-form',
-    imports: [FormsModule, CommonModule],
+    imports: [FormsModule],
     templateUrl: './immobilien-form.component.html',
     styleUrl: './immobilien-form.component.css'
 })
